c6929c49d5e09f240da7765a5dbbfba2329dfc52
Zachary T. Castellanos-Smith <ztsmith25@gmail.com>
2015-11-19 17:48:48 -0800

    implement twoSum function (linear time / linear space)

--- Javascript/twoSum.js
+++ Javascript/twoSum.js
@@ -9,3 +9,34 @@ Input: numbers={2, 7, 11, 15}, target=9
 Output: index1=1, index2=2
 */
 
+var twoSum = function(nums, target) {
+    var numbers = {}, duplicates = {}, sumSearch = null;
+    for(var i = 0; i < nums.length; i++){
+        if(numbers[nums[i]] || numbers[nums[i]] === 0){
+            duplicates[nums[i]] = i;
+        } else {
+            numbers[nums[i]] = i;
+        }
+    }
+    for(i = 0; i < nums.length; i++){
+        sumSearch = target - nums[i];
+        if (sumSearch === nums[i]){
+            if(duplicates[sumSearch] || duplicates[sumSearch] === 0){
+                if(duplicates[sumSearch] < i){
+                    return [duplicates[sumSearch] + 1, i + 1];
+                } else {
+                    return [i + 1, duplicates[sumSearch] + 1];
+                }
+            }
+        } else {
+            if(numbers[sumSearch] || numbers[sumSearch] === 0){
+                if(numbers[sumSearch] < i){
+                    return [numbers[sumSearch] + 1, i + 1];
+                } else {
+                    return [i + 1, numbers[sumSearch] + 1];
+                }
+            }
+        }
+        
+    }
+}; // linear time and linear space
