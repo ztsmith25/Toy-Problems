45cb48537c88fe443a27e2fe04260761bf159d20
Zachary T. Castellanos-Smith <ztsmith25@gmail.com>
2015-11-23 12:04:46 -0800

    code cleanup

--- Javascript/twoSum.js
+++ Javascript/twoSum.js
@@ -10,33 +10,35 @@ Output: index1=1, index2=2
 */
 
 var twoSum = function(nums, target) {
-    var numbers = {}, duplicates = {}, sumSearch = null;
-    for(var i = 0; i < nums.length; i++){
-        if(numbers[nums[i]] || numbers[nums[i]] === 0){
-            duplicates[nums[i]] = i;
-        } else {
-            numbers[nums[i]] = i;
-        }
+  var numbers = {},
+    duplicates = {},
+    sumSearch = null;
+  for (var i = 0; i < nums.length; i++) {
+    if (numbers[nums[i]] || numbers[nums[i]] === 0) {
+      duplicates[nums[i]] = i;
+    } else {
+      numbers[nums[i]] = i;
     }
-    for(i = 0; i < nums.length; i++){
-        sumSearch = target - nums[i];
-        if (sumSearch === nums[i]){
-            if(duplicates[sumSearch] || duplicates[sumSearch] === 0){
-                if(duplicates[sumSearch] < i){
-                    return [duplicates[sumSearch] + 1, i + 1];
-                } else {
-                    return [i + 1, duplicates[sumSearch] + 1];
-                }
-            }
+  }
+  for (i = 0; i < nums.length; i++) {
+    sumSearch = target - nums[i];
+    if (sumSearch === nums[i]) {
+      if (duplicates[sumSearch] || duplicates[sumSearch] === 0) {
+        if (duplicates[sumSearch] < i) {
+          return [duplicates[sumSearch] + 1, i + 1];
         } else {
-            if(numbers[sumSearch] || numbers[sumSearch] === 0){
-                if(numbers[sumSearch] < i){
-                    return [numbers[sumSearch] + 1, i + 1];
-                } else {
-                    return [i + 1, numbers[sumSearch] + 1];
-                }
-            }
+          return [i + 1, duplicates[sumSearch] + 1];
         }
-        
+      }
+    } else {
+      if (numbers[sumSearch] || numbers[sumSearch] === 0) {
+        if (numbers[sumSearch] < i) {
+          return [numbers[sumSearch] + 1, i + 1];
+        } else {
+          return [i + 1, numbers[sumSearch] + 1];
+        }
+      }
     }
+
+  }
 }; // linear time and linear space
