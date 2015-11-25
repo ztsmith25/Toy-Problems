da0a4ca0063139bc2f273b8a07def2f0040efb03
Zachary T. Castellanos-Smith <ztsmith25@gmail.com>
2015-11-24 20:56:54 -0800

    implement lengthOfLongestSubstring function

--- Javascript/lengthOfLongestSubstring.js
+++ Javascript/lengthOfLongestSubstring.js
@@ -5,5 +5,25 @@ which the length is 3. For "bbbbb" the longest substring is "b", with the length
 */
 
 var lengthOfLongestSubstring = function(s) {
- 
-};
+  var currentSubstringCache = {},
+    currentSubstring = s.charAt(0),
+    longestSubstring = null;
+  currentSubstringCache[s.charAt(0)] = s.charAt(0);
+  for (var i = 1; i < s.length; i++) {
+    if (!currentSubstringCache[s.charAt(i)]) {
+      currentSubstringCache[s.charAt(i)] = s.charAt(i);
+      currentSubstring += s.charAt(i);
+    } else {
+      if (!longestSubstring || currentSubstring > longestSubstring) {
+        longestSubstring = currentSubstring;
+      }
+      currentSubstring = '';
+      currentSubstringCache = {};
+    }
+  }
+  if (longestSubstring === null) {
+    return s.length;
+  } else {
+    return longestSubstring.length;
+  }
+}; //time complexity is O(N)
